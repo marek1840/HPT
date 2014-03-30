356d7537c36a467de78cc5f85cbe33df812120ed
Marek Żarnowski <marek1840@gmail.com>
2014-03-30 17:51:22 +0200

    implemented stock purchase and userdata routes

--- HPT/app/routes/userData.js
+++ HPT/app/routes/userData.js
@@ -1,17 +1,9 @@
 'use strict';
 
 // User routes use users controller
-var userData = require('../controllers/userData');
+var User = require('../controllers/userData');
 
 module.exports = function(app) {
-	//gets uset data (without stock) - email, capital
-	app.get('/users/:email', function(req, res){
-		return res.json('GET users/' + req.email);
-	});
-
-	//gets user stock
-	app.get('/users/:email/stock', function(req, res){
-		return res.json('GET users/' + req.email + '/stock');
-	});
-
+	app.get('/users/:email', User.data);
+    app.get('/users/:email/stock', User.owned);
 }
