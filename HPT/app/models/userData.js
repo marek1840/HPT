4ed7f2c93b430629c6f054986893edc1ad5f7a0f
Marek Żarnowski <marek1840@gmail.com>
2014-03-30 17:45:29 +0200

    Implemented UserData database queries

--- HPT/app/models/userData.js
+++ HPT/app/models/userData.js
@@ -3,17 +3,27 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
-var userDataSchema = new Schema({
+var UserDataSchema = new Schema({
 	email: {
 		type: String,
 		unique: true,
 		required: true
 	},
-	capital: Number,
+    capital: Number,
 	ownedStock: [{
 		company: String,
 		amount: Number
 	}]
 });
 
-mongoose.model('UserData', userDataSchema);
+UserDataSchema.statics.findAndModify = function (query, sort, doc, options, callback) {
+    return this.collection.findAndModify(query, sort, doc, options, callback);
+};
+
+UserDataSchema.statics.capitalOf = function(email){
+    return this.findOne({
+        email:email
+    }).capital;
+}
+
+mongoose.model('UserData', UserDataSchema);
