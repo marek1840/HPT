692563d947e0a4b43016f8b5e5519fa87a54a242
Marek Żarnowski <marek1840@gmail.com>
2014-03-30 17:47:24 +0200

    added temporary implementation for companies route

--- HPT/app/routes/company.js
+++ HPT/app/routes/company.js
@@ -3,19 +3,35 @@
 // User routes use users controller
 var companies = require('../controllers/companies');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-	//communicates with generator
-	//gets company data (current stock proce, brand, price change in %)
-	app.get('/company/:companyName', function(req, res){
-		return res.json('GET company/' + req.companyName);
-	});
+    app.get('/companies.json', function (req, res) {
+        return res.json([
+            {
+                name: 'c1',
+                industry: 'military',
+                difference: '100',
+                stockPrice: 150
+            },
+            {
+                name: 'c2',
+                industry: 'furniture',
+                difference: '200',
+                stockPrice: 12
+            }
+        ]);
+    });
+
+    //communicates with generator
+    //gets company data (current stock proce, brand, price change in %)
+    app.get('/company/:companyName', function (req, res) {
+        return res.json('got company');
+    });
 
 
-	// second iteration
-	//communicates with generator
-	// app.get('/:company/history', function(req, res))
+    // second iteration
+    //communicates with generator
+    // app.get('/:company/history', function(req, res))
 
 
-
-}
+};
