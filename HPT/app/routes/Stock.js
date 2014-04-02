3b1e6e504a4e9ae49f19f3f067e000ff386d5474
Marek Żarnowski <marek1840@gmail.com>
2014-04-02 10:31:57 +0200

    fixed naming issue

--- HPT/app/routes/Stock.js
+++ HPT/app/routes/Stock.js
@@ -2,13 +2,13 @@
 
 // User routes use users controller
 var companies = require('../controllers/companies');
-var Purchase = require('../controllers/Stock');
+var Stock = require('../controllers/Stock');
 
 
 module.exports = function(app) {
 	//communicates with generator
 	//adds purchased stock to user stock
-	app.post('/purchase', Purchase.purchase);
+	app.post('/purchase', Stock.purchase);
 
     app.post('/sell', function(req, res){
         return res.json('POST sell');
