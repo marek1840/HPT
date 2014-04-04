277a70b524afb9007f41b812ffaa9d1f918dc821
Marek Żarnowski <marek1840@gmail.com>
2014-04-04 19:34:05 +0200

    completed purchase controller implementation

--- HPT/app/controllers/Stock.js
+++ HPT/app/controllers/Stock.js
@@ -1,21 +1,91 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    UserData = mongoose.model('UserData')
+    UserData = require('./UserData'),
+    Companies = require('./companies');
 
 exports.purchase = function (req, res) {
+
+
     UserData.findOne({
         email: req.body.email
     }, 'capital -_id', function (err, userData) {
+
         if (err) {
-            return res.send('purchase err: ' + err);
+            return res.send('too few gold', 406);
         }
 
         if (userData.capital < req.body.cost) {
-            return res.send('not enough cash: ' + userData.capital)
+            return res.send('not enough cash: ' + cost)
         }
 
-        return res.send('OK')
+        Companies.buy(
+            {
+                company: req.body.company,
+                amount: req.body.amount
+            }, function (response) {
+                if (response !== 'OK') {
+                    return res.send(406)
+                }
+                else {
+                    UserData.updateCapital({
+                        email: req.body.email,
+                        amount: -req.body.cost
+                    }, function (err) {
+                        if (err) {
+                            return res.send('capital update failure', 500);
+                        } else {
+                            UserData.updateStock(
+                                {
+                                    email: req.body.email,
+                                    company: req.body.company,
+                                    amount: req.body.amount
+                                }, function (err) {
+                                    if (err) {
+                                        return res.send('stock update failure', 500)
+                                    } else {
+                                        return res.send(200);
+                                    }
+                                }
+                            );
+                        }
+                    })
 
+                }
+            });
     })
-}
+};
+
+exports.sell = function (req, res) {
+    Companies.sell(
+        {
+            company: req.body.company,
+            amount: req.body.amount
+        }, function (response) {
+            if (response !== 'OK') {
+                return res.send(406)
+            } else {
+                UserData.updateCapital({
+                    email: req.body.email,
+                    amount: req.body.income
+                }, function (err) {
+                    if (err) {
+                        return res.send('capital update failure', 500);
+                    } else {
+                        UserData.updateStock({
+                            email: req.body.email,
+                            company: req.body.company,
+                            amount: -req.body.amount
+                        }, function (err) {
+                            if (err) {
+                                return res.send('stock update failure', 500)
+                            } else {
+                                return res.send(200)
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    )
+};
