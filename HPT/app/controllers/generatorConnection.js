a3a060a718ed4f8bfd34926adad2f2988700938c
Marek Żarnowski <marek1840@gmail.com>
2014-04-04 19:35:05 +0200

    fixed minot bugs in communication with generator

--- HPT/app/controllers/generatorConnection.js
+++ HPT/app/controllers/generatorConnection.js
@@ -2,45 +2,48 @@
 
 var net = require('net');
 
-exports.getAllCompanies = function(callback) {
-    var client = net.connect({port: 8000}, function() {
-            client.write(JSON.stringify({"type": "getAll"}));
+exports.getAllCompanies = function (callback) {
+    var client = net.connect({port: 8000}, function () {
+        client.write(JSON.stringify({"type": "getAll"}));
     });
 
-    client.on('data', function(data) {
+    client.on('data', function (data) {
         callback(JSON.parse(data.toString()));
         client.end();
     });
 
-    client.on('end', function() {});
+    client.on('end', function () {
+    });
 };
 
-exports.buy = function(toBuy, callback) {
-    var client = net.connect({port: 8000}, function() {
+exports.buy = function (toBuy, callback) {
+    var client = net.connect({port: 8000}, function () {
         toBuy.type = "buy";
         client.write(JSON.stringify(toBuy));
     });
 
-    client.on('data', function(data) {
-        callback(JSON.parse(data.toString()));
+    client.on('data', function (data) {
+        callback(data.toString());
         client.end();
     });
 
-    client.on('end', function() {});
+    client.on('end', function () {
+    });
 };
 
-exports.sell = function(toSell, callback) {
-    var client = net.connect({port: 8000}, function() {
+exports.sell = function (toSell, callback) {
+    var client = net.connect({port: 8000}, function () {
         toSell.type = "sell";
         client.write(JSON.stringify(toSell));
     });
 
-    client.on('data', function(data) {
-        callback(JSON.parse(data.toString()));
+    client.on('data', function (data) {
+        callback(data.toString());
         client.end();
     });
 
-    client.on('end', function() {});
+    client.on('end', function () {
+    });
 };
 
 
