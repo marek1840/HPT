a5f9805e46ab3ea4374df25dfd3e38bf25a7a154
Marek Żarnowski <marek1840@gmail.com>
2014-04-04 19:36:55 +0200

    completed companies controller and routes

---
+++ HPT/app/controllers/companies.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var remote = require('./generatorConnection.js');
+
+exports.getAll = function (req, res) {
+    remote.getAllCompanies(function (companies) {
+        return res.json(companies);
+    })
+};
+
+exports.buy = remote.buy;
+
+exports.sell = remote.sell;
--- HPT/app/routes/company.js
+++ HPT/app/routes/company.js
@@ -1,33 +1,11 @@
 'use strict';
 
 // User routes use users controller
-var companies = require('../controllers/companies');
+var Companies = require('../controllers/companies');
 
 module.exports = function (app) {
 
-    app.get('/companies.json', function (req, res) {
-        return res.json([
-            {
-                name: 'c1',
-                industry: 'military',
-                difference: '100',
-                stockPrice: 150
-            },
-            {
-                name: 'c2',
-                industry: 'furniture',
-                difference: '200',
-                stockPrice: 12
-            }
-        ]);
-    });
-
-    //communicates with generator
-    //gets company data (current stock proce, brand, price change in %)
-    app.get('/company/:companyName', function (req, res) {
-        return res.json('got company');
-    });
-
+    app.get('/companies.json', Companies.getAll);
 
     // second iteration
     //communicates with generator
