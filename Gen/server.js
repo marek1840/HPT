4dfc885804e1b83ab49f0a3be640607bc3d57943
Marek Żarnowski <marek1840@gmail.com>
2014-04-04 19:35:36 +0200

    minor alteration in generator server

--- Gen/server.js
+++ Gen/server.js
@@ -1,16 +1,33 @@
 var net = require('net'),
     Company = require('./model/Companies.js');
 
+var dummyCompanies = [
+    {
+        name: 'c1',
+        industry: 'military',
+        change: -100,
+        stockPrice: 150
+    },
+    {
+        name: 'c2',
+        industry: 'furniture',
+        change: 100,
+        stockPrice: 12
+    }
+]
+
 var server = net.createServer(function(c) {
     c.on('data', function(data) {
         var received = JSON.parse(data.toString());
-        console.log(received);
+
+        console.log('got ' + JSON.stringify(received))
+
         if (received.type === "getAll") {
             //here you are sending all data to the user like this:
-            c.write(JSON.stringify({"GOTIT": "getAll"}));
+            c.write(JSON.stringify(dummyCompanies));
         } else if(received.type === "buy"){
             //purchases here
-            c.write(JSON.stringify({"GOTIT": "buy"}));
+            c.write(('OK'));
         } else if(received.type === "sell") {
             //sells here
             c.write(JSON.stringify({"GOTIT": "sell"}));
