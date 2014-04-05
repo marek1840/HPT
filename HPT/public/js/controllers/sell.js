6a7dfeabe3f625bc7b36b5333846f0ac86b8fe2c
Marek Żarnowski <marek1840@gmail.com>
2014-04-05 13:35:47 +0200

    minor syntactic fix

--- HPT/public/js/controllers/sell.js
+++ HPT/public/js/controllers/sell.js
@@ -7,13 +7,13 @@ angular.module('mean.system').controller('SellController',
         $scope.industryPrefix = '';
 
         var current_user = $scope.global.user.email;
-        $http.get('/users/'+current_user+'/stock').success(function (ownedStock) { 
+        $http.get('/users/' + current_user + '/stock').success(function (ownedStock) {
             $http.get('/companies.json').success(function (companies) {
                 $scope.industries = companies.reduce(function (acc, company) {
                     if (company.name in ownedStock)
                         acc[company.industry] = true;
                     return acc;
-                    }, {});
+                }, {});
 
                 $scope.ind = function () {
                     var keys = [];
@@ -23,18 +23,18 @@ angular.module('mean.system').controller('SellController',
                 };
 
                 $scope.ownedCompanies = companies.filter(function (company) {
-                    company.ownedStock = ownedStock[company.name]
-                    return ownedStock[company.name] > 0; 
+                    company.ownedStock = ownedStock[company.name];
+                    return ownedStock[company.name] > 0;
                 });
 
                 $scope.companies = function () {
                     return $scope.ownedCompanies.filter(function (company) {
-                        return $scope.industries[company.industry]; 
+                        return $scope.industries[company.industry];
                     });
                 };
             });
         });
-	
+
 
         $scope.sold = {};
 
@@ -49,10 +49,10 @@ angular.module('mean.system').controller('SellController',
                             company: companyName,
                             amount: amount,
                             cost: cost
-                        }).success(function (data) {
+                        }).success(function () {
                             $scope.sold[companyName] = amount;
                         });
-                    }
+                    };
                 }
             };
         };
