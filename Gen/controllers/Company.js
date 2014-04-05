617e04e65e2dfd7ba6c36d2d7304ade1c487a6a3
Marek Żarnowski <marek1840@gmail.com>
2014-04-05 13:26:01 +0200

    added communication with database in generator

---
+++ Gen/controllers/Company.js
@@ -0,0 +1,48 @@
+'use strict';
+var mongoose = require('mongoose'),
+    Company = mongoose.model('Company');
+
+exports.getAll = function (callback) {
+    Company.getAll(function (err, data) {
+        if (err) {
+            return callback([]);
+        }
+        return callback(data);
+    })
+};
+
+exports.buy = function (buyData, callback) {
+    Company.getStockAmount(buyData.company, function (err, companyData) {
+        if (err) {
+            return callback('ERROR');
+        }
+        console.log('buying: ' + buyData.amount + ' avaiable ' + companyData.stockAmount)
+
+        if (!companyData.stockAmount || companyData.stockAmount < buyData.amount) {
+            return callback('REJECT');
+        }
+        Company.findAndModify({name: buyData.company},
+            [],
+            {$inc: {stockAmount: -buyData.amount}},
+            function (err) {
+                if (err) {
+                    return callback('REJECT');
+                }
+
+                return callback('OK');
+            });
+    })
+};
+
+exports.sell = function (buyData, callback) {
+    Company.findAndModify({name: buyData.company},
+        [],
+        {$inc: {stockAmount: buyData.amount}},
+        function (err) {
+            if (err) {
+                return callback('REJECT');
+            }
+
+            return callback('OK');
+        });
+};
--- Gen/server.js
+++ Gen/server.js
@@ -1,42 +1,39 @@
+'use strict';
+require('./model/Companies.js');
+
 var net = require('net'),
-    Company = require('./model/Companies.js');
+    Company = require('./controllers/Company.js');
 
-var dummyCompanies = [
-    {
-        name: 'c1',
-        industry: 'military',
-        change: 100,
-        stockPrice: 150
-    },
-    {
-        name: 'c2',
-        industry: 'furniture',
-        change: 100,
-        stockPrice: 12
+
+var server = net.createServer(function (c) {
+    var sendData = function (data) {
+        if (typeof data !== 'string') {
+            return c.write(JSON.stringify(data));
+        } else {
+            return c.write(data);
+        }
     }
-]
 
-var server = net.createServer(function(c) {
-    c.on('data', function(data) {
+    c.on('data', function (data) {
         var received = JSON.parse(data.toString());
 
         console.log('got ' + JSON.stringify(received))
 
         if (received.type === "getAll") {
-            //here you are sending all data to the user like this:
-            c.write(JSON.stringify(dummyCompanies));
-        } else if(received.type === "buy"){
-            //purchases here
-            c.write(('OK'));
-        } else if(received.type === "sell") {
-            //sells here
-            c.write(JSON.stringify({"GOTIT": "sell"}));
+            Company.getAll(sendData);
+        } else if (received.type === "buy") {
+            console.log("entering buy");
+            Company.buy(received, sendData);
+            console.log("leaving buy");
+        } else if (received.type === "sell") {
+            Company.sell(received, sendData);
         }
     });
-    c.on('end', function() {});
+    c.on('end', function () {
+    });
 });
 
-server.listen(8000, function() {
+server.listen(8000, function () {
     console.log('server bound');
 });
 
