65519ec5dc5989f4da80a903ad8fa56ec8cfbcae
pimucha <pimucha@student.agh.edu.pl>
2014-04-05 13:00:39 +0200

    Fix simulation.

--- Gen/simulation/simulation.js
+++ Gen/simulation/simulation.js
@@ -1,45 +1,19 @@
-function randomInt (low, high) {
-    return Math.floor(Math.random() * (high - low) + low);
+function random (low, high) {
+  return Math.random() * (high - low) + low;
 }
 
-function random (low, high) {
-    return Math.random() * (high - low) + low;
+function generateNewStockPrice(stockPrice) {
+  return stockPrice * random(0.75, 1,25);
 }
+
+var Company = require('./../model/Companies.js');
 
 var simulation = function() {
-	var MongoClient = require('mongodb').MongoClient;
-	
-	MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {		//mongodb database address
-		if(!err) {
-		  db.collection('test', {}, function(err, collection) {		//collection name
-  		  var cursor = collection.find();
-  		  cursor.each(function(err, item) {
-  		    if(item!==null) {
-  		      switch(randomInt(1, 4)) {
-  		        case 1:
-  		          collection.update({name:item.name}, {$set:{stockPrice:Math.floor(random(0.8, 0.99)*item.stockPrice)}}, {w: 1}, function(err, numberUpdated) {
-  		            if(!err)
-  		              db.close();
-  		            else
-  		              return console.dir(err);
-  		           });
-  		          break;
-  		         case 2:
-  		           collection.update({name:item.name}, {$set:{stockPrice:Math.floor(random(1.01, 1.2)*item.stockPrice)}}, {w: 1}, function(err, numberUpdated) {
-  		             if(!err)
-  		               db.close();
-  		             else
-  		               return console.dir(err);
-  		            });
-  		           break;
-  		         default:
-  		      }
-  		    }
-  		   });
-		  });
-		} else
-		  return console.dir(err);
-	});
-};
+  Company.getAll(function(companies) {
+    for(var company in companies){
+      company.stockPrice= generateNewStockPrice(company.stockPrice)
+      company.save()
+    }
+});
 
 module.exports.simulation = simulation;
