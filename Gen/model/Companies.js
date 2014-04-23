94eb69a9581c3d769a95aa2f1a3a35575e8d4017
wojtek132 <wojtek132@users.noreply.github.com>
2014-04-23 13:21:51 +0200

    Add function deleting all companies (need for populate.js).

--- Gen/model/Companies.js
+++ Gen/model/Companies.js
@@ -7,7 +7,11 @@ var CompanySchema = new Schema({
     name: String,
     industry: String,
     stockPrice: Number,
-    stockAmount: Number
+    stockAmount: Number,
+    history: [{
+    	price: Number,
+    	date: Date //must be sorted
+    }]
 });
 
 CompanySchema.statics.getAll = function (callback) {
@@ -20,6 +24,10 @@ CompanySchema.statics.getStockAmount = function (companyName, callback) {
 
 CompanySchema.statics.findAndModify = function (query, sort, doc, options, callback) {
     return this.collection.findAndModify(query, sort, doc, options, callback);
+};
+
+CompanySchema.statics.removeAll = function (callback) {
+    this.remove({}, callback);
 };
 
 var company = mongoose.model('Company', CompanySchema);
