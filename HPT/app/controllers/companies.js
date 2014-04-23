9f141d6c7d55bfb3f592e834526ff744083abdbf
wojtek132 <wojtek132@users.noreply.github.com>
2014-04-23 13:29:19 +0200

    Added function getAllWithLastChange

--- HPT/app/controllers/companies.js
+++ HPT/app/controllers/companies.js
@@ -8,6 +8,28 @@ exports.getAll = function (req, res) {
     });
 };
 
+exports.getAllWithLastChange = function (req, res) {
+    remote.getAllCompanies(function (companies) {
+    	console.log(companies);
+    	companies.map( function(company) {
+    		//console.log(company);
+    		if (!(company.history)) {
+    			console.log("no history");
+    		} else {
+    			//console.log(company.history);
+    			previousPrice = company.history[0].price;
+	    		company.change = (company.stockPrice - previousPrice)/previousPrice
+	    		delete json.history;
+	    		console.log(company);
+	    	}
+    		return company;
+    	});
+    	console.log("send companies with last change");
+    	//niekiedy blad przy ponownym wysylaniu?
+        return res.json(companies);
+    });
+};
+
 exports.buy = remote.buy;
 
 exports.sell = remote.sell;
