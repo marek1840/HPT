c51f25bd5d03522acfdaf6544fafbac233501f3c
Dawid Szmigielski <althardaine@gmail.com>
2014-05-04 15:19:22 +0200

    Added function and it's attachement to the button (still no broker working)

--- HPT/public/js/controllers/brokers.js
+++ HPT/public/js/controllers/brokers.js
@@ -1,14 +1,29 @@
 'use strict';
 
+//import broker
+
 angular.module('mean.system').controller('BrokersController',
     ['$scope', '$http', '$location', 'Global', 
 	function ($scope, $http, $location, Global) {
         $scope.global = Global;
         var current_user = $scope.global.user.email;
         $scope.type = 'allBroker';
+        $scope.list= [];
+        $scope.failure = 
 	
 		$scope.startBroker = function () {
-			
+			var amount = $scope.amount;
+			var time = $scope.time;
+			var profit = $scope.profit;
+			if(amount != null && time != null && profit != null) {
+				if ($scope.type == 'allBroker') {
+					var broker = new Broker(current_user, amount, strategies.buyAll, time * 1000, profit);
+				} else {
+					var broker = new Broker(current_user, amount, strategies.buyMostStocks, time * 1000, profit);
+				}
+				broker.start();
+				setInterval(broker.getData, 3000);
+			}
 		}
 
     }]);
